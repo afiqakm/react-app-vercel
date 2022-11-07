df2d5a8d1fa04fe1b43843140b4e3b52d01da5a7
BuildTools <unconfigured@null.spigotmc.org>
2022-11-08 00:32:08 +0800

    testing tab style

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {ReactComponent as Logo } from '../assets/images/afiq.svg'
+
+export default function Hero() {
+  return (
+    <div>
+      <div className=" m-auto flex inset-x-0 top-0 w-40 pt-10">
+        <Logo/>
+      </div>
+    </div>
+  )
+}
