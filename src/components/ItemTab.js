377eba44c65277b9e6526edd0624b15b290afefc
afiqakm <afiq98akmal@gmail.com>
2022-11-08 17:10:28 +0800

    adding item in tab

--- src/components/ItemTab.js
+++ src/components/ItemTab.js
@@ -1,26 +1,26 @@
-import { useState } from 'react'
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { useState,useEffect } from 'react'
 import { Tab } from '@headlessui/react'
+import AddTabButton from './AddTabButton'
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
 export default function ItemTab() {
+
+    const [isSent, setIsSent] = useState(false);
+    
     let [categories] = useState({
         Personal: [
             {
                 id: 1,
                 title: 'Does drinking coffee make you smarter?',
-                date: '5h ago',
-                commentCount: 5,
-                shareCount: 2,
             },
             {
                 id: 2,
                 title: "So you've bought coffee... now what?",
-                date: '2h ago',
-                commentCount: 3,
-                shareCount: 2,
             },
         ],
         Work: [
@@ -31,10 +31,21 @@ export default function ItemTab() {
         ],
     })
 
-    return (
-        <div className=" items-center w-full max-w-xl px-2 py-16 sm:px-0">
+    let [newCategories] = useState({
+        id: 3,
+        title: "testing whether it can work or not",
+    })
+
+    function handlePlusClick() {
+        setIsSent(true);
+        categories.Personal.push(newCategories);
+        console.log(JSON.stringify(categories));
+      }
+      if (isSent) {
+        <div className="items-center w-full max-w-xl px-2 py-16 sm:px-0">
             <Tab.Group>
-                <Tab.List className=" tabs tabs-boxed ">
+                <div className='flex flex-row justify-between'>
+                <Tab.List className=" tabs tabs-boxed object-fit w-fit">
                     {Object.keys(categories).map((category) => (
                         <Tab
                             key={category}
@@ -51,6 +62,9 @@ export default function ItemTab() {
                         </Tab>
                     ))}
                 </Tab.List>
+                <AddTabButton/>
+                </div>
+                
                 <Tab.Panels className="mt-2">
                     {Object.values(categories).map((posts, idx) => (
                         <Tab.Panel
@@ -70,13 +84,66 @@ export default function ItemTab() {
                                             {post.title}
                                         </h3>
 
-                                        <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
-                                            <li>{post.date}</li>
-                                            <li>&middot;</li>
-                                            <li>{post.commentCount} comments</li>
-                                            <li>&middot;</li>
-                                            <li>{post.shareCount} shares</li>
-                                        </ul>
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                'absolute inset-0 rounded-md',
+                                                'ring-blue-400 focus:z-10 focus:outline-none focus:ring-2'
+                                            )}
+                                        />
+                                    </li>
+                                ))}
+                            </ul>
+                            
+                            <button className='btn btn-primary w-full' onClick={handlePlusClick}>Add Item</button>
+                        </Tab.Panel>
+                    ))}
+                </Tab.Panels>
+            </Tab.Group>
+        </div>
+      }
+    return (
+        <div className="items-center w-full max-w-xl px-2 py-16 sm:px-0">
+            <Tab.Group>
+                <div className='flex flex-row justify-between'>
+                <Tab.List className=" tabs tabs-boxed object-fit w-fit">
+                    {Object.keys(categories).map((category) => (
+                        <Tab
+                            key={category}
+                            className={({ selected }) =>
+                                classNames(
+                                    'tab tab-lg',
+                                    selected
+                                        ? 'tab-active'
+                                        : ''
+                                )
+                            }
+                        >
+                            {category}
+                        </Tab>
+                    ))}
+                </Tab.List>
+                <AddTabButton/>
+                </div>
+                
+                <Tab.Panels className="mt-2">
+                    {Object.values(categories).map((posts, idx) => (
+                        <Tab.Panel
+                            key={idx}
+                            className={classNames(
+                                'rounded-xl bg-white p-3',
+                                'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
+                            )}
+                        >
+                            <ul>
+                                {posts.map((post) => (
+                                    <li
+                                        key={post.id}
+                                        className="relative rounded-md p-3 hover:bg-gray-100"
+                                    >
+                                        <h3 className="text-sm font-medium leading-5">
+                                            {post.title}
+                                        </h3>
 
                                         <a
                                             href="#"
@@ -88,6 +155,8 @@ export default function ItemTab() {
                                     </li>
                                 ))}
                             </ul>
+                            
+                            <button className='btn btn-primary w-full' onClick={handlePlusClick}>Add Item</button>
                         </Tab.Panel>
                     ))}
                 </Tab.Panels>
